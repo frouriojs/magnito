5b0620b15bc5fd8dfc4f431cfeb718147ffd2ac2
solufa <solufa2020@gmail.com>
2023-07-10 22:58:28 +0900

    chore: set output mode

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx'],
   basePath: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '',
+  output: 'export',
   trailingSlash: true,
   eslint: { ignoreDuringBuilds: true },
 };
