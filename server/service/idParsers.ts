f7be29177d1c2703c6f10f98482932e0db511999
solufa <solufa2020@gmail.com>
2023-08-19 22:24:55 +0900

    refactor: update zod@3.22

--- server/service/idParsers.ts
+++ server/service/idParsers.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 import type { TaskId, UserId } from '../commonTypesWithClient/branded';
 
-export const userIdParser: z.ZodType<UserId> = z.string().brand<'UserId'>();
+const createIdParser = <T extends string>() => z.string() as unknown as z.ZodType<T>;
 
-export const taskIdParser: z.ZodType<TaskId> = z.string().brand<'TaskId'>();
+export const userIdParser = createIdParser<UserId>();
+
+export const taskIdParser = createIdParser<TaskId>();
