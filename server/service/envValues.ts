c16d60495712bede84084c660878408fdd693e26
solufa <solufa2020@gmail.com>
2023-10-26 11:28:28 +0900

    refactor: validate server env

--- server/service/envValues.ts
+++ server/service/envValues.ts
@@ -1,22 +1,24 @@
 import dotenv from 'dotenv';
+import { z } from 'zod';
 
 dotenv.config();
 
-const PORT = +(process.env.PORT ?? '8080');
-const API_BASE_PATH = process.env.API_BASE_PATH ?? '';
-const API_ORIGIN = process.env.API_ORIGIN ?? '';
-const CORS_ORIGIN = process.env.CORS_ORIGIN ?? '';
-const FIREBASE_AUTH_EMULATOR_HOST = process.env.FIREBASE_AUTH_EMULATOR_HOST;
-const FIREBASE_SERVER_KEY = process.env.FIREBASE_SERVER_KEY ?? '';
-const S3_ENDPOINT = process.env.S3_ENDPOINT ?? '';
-const S3_BUCKET = process.env.S3_BUCKET ?? '';
-const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY ?? '';
-const S3_SECRET_KEY = process.env.S3_SECRET_KEY ?? '';
-const S3_REGION = process.env.S3_REGION ?? '';
+const PORT = +z.string().regex(/^\d+$/).parse(process.env.PORT);
+const API_BASE_PATH = z.string().startsWith('/').parse(process.env.API_BASE_PATH);
+const CORS_ORIGIN = z.string().url().parse(process.env.CORS_ORIGIN);
+const FIREBASE_AUTH_EMULATOR_HOST = z
+  .string()
+  .optional()
+  .parse(process.env.FIREBASE_AUTH_EMULATOR_HOST);
+const FIREBASE_SERVER_KEY = z.string().parse(process.env.FIREBASE_SERVER_KEY);
+const S3_ENDPOINT = z.string().parse(process.env.S3_ENDPOINT ?? '');
+const S3_BUCKET = z.string().parse(process.env.S3_BUCKET ?? '');
+const S3_ACCESS_KEY = z.string().parse(process.env.S3_ACCESS_KEY ?? '');
+const S3_SECRET_KEY = z.string().parse(process.env.S3_SECRET_KEY ?? '');
+const S3_REGION = z.string().parse(process.env.S3_REGION ?? '');
 
 export {
   API_BASE_PATH,
-  API_ORIGIN,
   CORS_ORIGIN,
   FIREBASE_AUTH_EMULATOR_HOST,
   FIREBASE_SERVER_KEY,
