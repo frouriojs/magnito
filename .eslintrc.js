aa085e6807c49f9fe2ed54f2792b33104e3750f1
solufa <solufa2020@gmail.com>
2024-01-28 21:32:16 +0900

    chore: add no-explicit-any rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-non-null-assertion': 'error',
     '@typescript-eslint/strict-boolean-expressions': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
     complexity: ['error', 5],
     'max-depth': ['error', 2],
     'max-nested-callbacks': ['error', 3],
