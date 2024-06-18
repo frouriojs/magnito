cb6eac5d99fa52276f90bb1dec06127228224a12
caru-ini <s1f102400392@iniad.org>
2024-06-18 20:06:02 +0900

    refactor: extract genVerifier function in genCredentials

--- server/domain/user/service/genCredentials.ts
+++ server/domain/user/service/genCredentials.ts
@@ -3,6 +3,20 @@ import crypto from 'crypto';
 import { g, N, Nbytes } from './srp/constants';
 import { calculatePrivateKey, getPoolName, toBufferWithLength } from './srp/util';
 
+export const genVerifier = (params: {
+  poolId: EntityId['userPool'];
+  username: string;
+  password: string;
+  salt: string;
+}): string => {
+  // extract pool name from poolId (poolId format: userPoolId_poolName)
+  const poolName = getPoolName(params.poolId);
+  const privateKey = calculatePrivateKey(poolName, params.username, params.password, params.salt);
+  // verifier = g ^ privateKey % N
+  const verifier = toBufferWithLength(g.modPow(privateKey, N), Nbytes).toString('hex');
+  return verifier;
+};
+
 export const genCredentials = (params: {
   poolId: EntityId['userPool'];
   username: string;
@@ -10,10 +24,6 @@ export const genCredentials = (params: {
   salt?: string;
 }): { salt: string; verifier: string } => {
   const salt = params.salt || crypto.randomBytes(16).toString('hex');
-  // extract pool name from poolId (poolId format: userPoolId_poolName)
-  const poolName = getPoolName(params.poolId);
-  const privateKey = calculatePrivateKey(poolName, params.username, params.password, salt);
-  // verifier = g^privateKey % N
-  const verifier = toBufferWithLength(g.modPow(privateKey, N), Nbytes).toString('hex');
+  const verifier = genVerifier({ ...params, salt });
   return { salt, verifier };
 };
