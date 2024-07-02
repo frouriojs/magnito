a939e17394edeafdc75cd7290231b96bafdccce4
caru-ini <caru17122@gmail.com>
2024-07-02 23:57:15 +0900

    fix: regenerate if the random value is invalid

--- server/domain/user/service/srp/calcSrpB.ts
+++ server/domain/user/service/srp/calcSrpB.ts
@@ -9,10 +9,17 @@ export const calculateSrpB = (
   b: string;
   B: string;
 } => {
-  const b = crypto.randomBytes(32);
-  const bInt = fromBuffer(b);
+  let BInt = BigInteger.ZERO;
+  let b = Buffer.from([0]);
   const vInt = new BigInteger(v, 16);
+
+  while (BInt === BigInteger.ZERO) {
+    b = crypto.randomBytes(32);
+    const bInt = fromBuffer(b);
+    BInt = multiplierParam.multiply(vInt).add(g.modPow(bInt, N)).mod(N);
+  }
+
   // kv + g^b
-  const B = toBuffer(multiplierParam.multiply(vInt).add(g.modPow(bInt, N)).mod(N));
+  const B = toBuffer(BInt);
   return { b: b.toString('hex'), B: B.toString('hex') };
 };
