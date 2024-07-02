dd81155bb953de2118778d084ce638f750dc15d5
caru-ini <caru17122@gmail.com>
2024-07-02 23:56:30 +0900

    refactor: remove unnecessary assertions

--- server/domain/user/service/srp/calcSessionKey.ts
+++ server/domain/user/service/srp/calcSessionKey.ts
@@ -1,4 +1,3 @@
-import assert from 'assert';
 import crypto from 'crypto';
 import { BigInteger } from 'jsbn';
 import { N, Nbytes } from './constants';
@@ -19,14 +18,8 @@ export const calculateSessionKey = (params: {
   v: string;
 }): Buffer => {
   const Aint = new BigInteger(padHex(params.A), 16);
-  const Bint = new BigInteger(padHex(params.B), 16);
   const bInt = new BigInteger(padHex(params.b), 16);
   const vInt = new BigInteger(params.v, 16);
-
-  assert(Aint.compareTo(BigInteger.ZERO) > 0, 'A should be greater than 0');
-  assert(Aint.compareTo(N) < 0, 'A should be less than N');
-  assert(Bint.compareTo(BigInteger.ZERO) > 0, 'B should be greater than 0');
-  assert(Bint.compareTo(N) < 0, 'A should be less than N');
 
   const scramblingParameter = calculateScramblingParameter(params.A, params.B);
 
