d430a65dba6a2718f280a7a6d66c6db3a4c5f5ad
solufa <solufa2020@gmail.com>
2024-07-13 00:31:17 +0900

    refactor: move endpoint to server/index.ts

--- server/scripts/config.common.js
+++ server/scripts/config.common.js
@@ -2,7 +2,7 @@ const { nodeExternalsPlugin } = require('esbuild-node-externals');
 const path = require('path');
 
 module.exports = {
-  entryPoints: [path.resolve(__dirname, '../entrypoints/index.ts')],
+  entryPoints: [path.resolve(__dirname, '../index.ts')],
   outdir: path.resolve(__dirname, '../'),
   platform: 'node',
   target: 'node20',
